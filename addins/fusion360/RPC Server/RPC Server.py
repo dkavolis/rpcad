d4199525836f21b7d757840ca68b1ef2c4e3e91a
dkavolis <12998363+dkavolis@users.noreply.github.com>
2021-07-07 04:52:06 +0100

    log rpcad/server to file

--- addins/fusion360/RPC Server/RPC Server.py
+++ addins/fusion360/RPC Server/RPC Server.py
@@ -6,13 +6,17 @@ import sys
 import traceback
 from threading import Thread
 from typing import Optional
+import time
+import logging
+from logging import handlers
 
 import adsk.cam
 import adsk.core
 import adsk.fusion
 import adsk
 
-sys.path.append(os.path.dirname(__file__))
+addin_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(addin_dir)
 
 
 SERVER = None
@@ -20,13 +24,18 @@ PROCESS: Optional[Thread] = None
 EVENTS = None
 
 
+logger = logging.getLogger("rpcad.Fusion360.AddIn")
+handler = handlers.RotatingFileHandler(
+    f"{addin_dir}/service.log",
+    mode="a+",
+    backupCount=5,
+    delay=False,
+    maxBytes=1024 * 1024,
+)
+
+
 def start_service():
-    from rpcad import reload_service_modules, fusion
-
-    # make sure any changes are picked up by fusion 360 on restart since the
-    # embedded python interpreter does not unload modules by default
-    reload_service_modules(fusion)
-
+    from rpcad import RPCAD_LOGLEVEL
     from rpcad.fusion import Fusion360Service
     from rpyc.utils.server import ThreadPoolServer
 
@@ -35,8 +44,20 @@ def start_service():
     # use requestBatchSize = 1 since most CAD functions can take a while to
     # complete, especially when they have to use events to communicate and wait
     # for the main thread to finish
-    SERVER = Fusion360Service.create_server(server=ThreadPoolServer, requestBatchSize=1)
-    SERVER.start()
+    try:
+        SERVER = Fusion360Service.create_server(
+            server=ThreadPoolServer, requestBatchSize=1
+        )
+        SERVER.logger.addHandler(handler)
+        SERVER.logger.setLevel(RPCAD_LOGLEVEL)
+        logger.info("Server created at %s:%s, starting", SERVER.host, SERVER.port)
+        SERVER.start()
+    except Exception:
+        logger.exception("RPC Server failed")
+        app: adsk.core.Application = adsk.core.Application.get()  # type: ignore
+        ui = app.userInterface
+        if ui:
+            ui.messageBox("Failed to start RPC server:\n{}".format(traceback.format_exc()))  # type: ignore
 
 
 def run(context):
@@ -45,27 +66,44 @@ def run(context):
         app: adsk.core.Application = adsk.core.Application.get()  # type: ignore
         ui = app.userInterface
 
-        sys.path.append(os.path.dirname(os.path.abspath(__file__)))
         import rpcad
-        import importlib
 
-        importlib.reload(rpcad)
-
+        rpcad = rpcad.reload()
         from rpcad.fusion import register_events
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+
+        root_logger = logging.getLogger(rpcad.__name__)
+        root_logger.addHandler(handler)
+        root_logger.setLevel(rpcad.RPCAD_LOGLEVEL)
+        handler.setLevel(rpcad.RPCAD_LOGLEVEL)
+
+        logger.info("Starting RPC server addin")
 
         global EVENTS
         EVENTS = register_events()
+        logger.info("Registered custom events")
 
-        PROCESS = Thread(target=start_service, daemon=True)
+        PROCESS = Thread(target=start_service, daemon=False)
         PROCESS.start()
 
+        time.sleep(0.05)
+
         # need to hold references to events and handlers so that they don't go
-        # out of scope
-        adsk.autoTerminate(False)
+        # out of scope unless server failed to start
+        if PROCESS.is_alive():
+            adsk.autoTerminate(False)
+        else:
+            adsk.terminate()
 
     except:  # noqa: E722
         if ui:
             ui.messageBox("Failed:\n{}".format(traceback.format_exc()))  # type: ignore
+    finally:
+        handler.flush()
 
 
 def stop(context):
@@ -76,6 +114,8 @@ def stop(context):
 
         if SERVER is not None:
             SERVER.close()
+            logger.info("Server closed")
+            SERVER.logger.removeHandler(handler)
 
         if PROCESS is not None:
             PROCESS.join()
@@ -84,7 +124,12 @@ def stop(context):
             from rpcad.fusion import unregister_events
 
             unregister_events(EVENTS.values())
+            logger.info("Events unregistered")
 
     except:  # noqa: E722
         if ui:
             ui.messageBox("Failed:\n{}".format(traceback.format_exc()))  # type: ignore
+    finally:
+        handler.flush()
+        handler.close()
+        logging.getLogger("rpcad").removeHandler(handler)
