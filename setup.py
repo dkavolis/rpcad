3ada26916aeef4df5f7ac2e6273ef1a0ca9d0676
dkavolis <12998363+dkavolis@users.noreply.github.com>
2021-07-07 15:57:11 +0100

    use Path instead of str make sure Fusion360 rpyc version is the same as current python skip Fusion360 python directories without python executables

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ import sys
 import os
 from subprocess import check_call
 import shutil
+from pathlib import Path
 
 from pkg_resources import VersionConflict, require
 from setuptools import setup
@@ -37,8 +38,10 @@ class PostInstallCommand(install):
 
     def install_win(self):
         self.install_fusion360(
-            os.path.expandvars("%LocalAppData%/Autodesk/webdeploy/production"),
-            os.path.expandvars("%AppData%/Autodesk/Autodesk Fusion 360/API/AddIns"),
+            Path(os.path.expandvars("%LocalAppData%/Autodesk/webdeploy/production")),
+            Path(
+                os.path.expandvars("%AppData%/Autodesk/Autodesk Fusion 360/API/AddIns")
+            ),
         )
 
     def install_mac(self):
@@ -51,29 +54,44 @@ class PostInstallCommand(install):
         # potentially FreeCAD
         pass
 
-    def install_fusion360(self, fusion360_dir: str, addin_dir: str) -> None:
-        source_dir = os.path.abspath(os.path.dirname(__file__))
-        requirements = os.path.join(source_dir, "requirements.txt")
+    def install_fusion360(self, fusion360_dir: Path, addin_dir: Path) -> None:
+        import rpyc
+
+        rpyc_version = rpyc.version.version_string  # type: ignore
+
+        source_dir = Path(__file__).absolute().parent
         print("-- Looking for python executables in ", fusion360_dir)
-        for python_dir in glob.glob(f"{fusion360_dir}/**/Python"):
-            print("-- Installing rpyc for ", python_dir)
+        for python_exe in glob.glob(f"{fusion360_dir}/**/Python/[Pp]ython*"):
+            executable = Path(python_exe)
+            python_dir = executable.parent
+            if executable.suffix in (".dll", ".so", ".dylib"):
+                # not an executable
+                continue
+
+            print(f"-- Installing rpyc=={rpyc_version} for {python_dir}")
             check_call(
-                ["python", "-m", "pip", "install", "-r", requirements, "--user"],
+                [
+                    executable,
+                    "-m",
+                    "pip",
+                    "install",
+                    "--upgrade",
+                    f"rpyc=={rpyc_version}",
+                    "--user",
+                ],
                 cwd=python_dir,
             )
 
-        dst = os.path.join(addin_dir, "RPC Server")
+        dst = addin_dir / "RPC Server"
         if os.path.exists(dst):
             shutil.rmtree(dst)
 
-        print("-- Copying addin to ", addin_dir)
+        print(f"-- Copying addin to {addin_dir}")
         addin = shutil.copytree(
-            os.path.join(source_dir, "addins", "fusion360", "RPC Server"),
-            os.path.join(addin_dir, "RPC Server"),
+            source_dir / "addins" / "fusion360" / "RPC Server",
+            addin_dir / "RPC Server",
         )
-        shutil.copytree(
-            os.path.join(source_dir, "src", "rpcad"), os.path.join(addin, "rpcad")
-        )
+        shutil.copytree(source_dir / "src" / "rpcad", addin / "rpcad")
 
 
 try:
