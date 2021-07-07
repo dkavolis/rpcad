769806a89bb134e1ff675f42122645a1e65425cc
dkavolis <12998363+dkavolis@users.noreply.github.com>
2021-07-07 01:06:02 +0100

    update fusion addin to run server in a worker thread

--- addins/fusion360/RPC Server/RPC Server.py
+++ addins/fusion360/RPC Server/RPC Server.py
@@ -4,63 +4,74 @@
 import os
 import sys
 import traceback
-
-# from threading import Thread
+from threading import Thread
+from typing import Optional
 
 import adsk.cam
 import adsk.core
 import adsk.fusion
+import adsk
 
 sys.path.append(os.path.dirname(__file__))
 
 
 SERVER = None
-PROCESS = None
-
-# Fusion360 doesn't like it's API called from worker threads so have to use plugin with
-# a server in main thread, have to relaunch server for new connections
-# Threaded server works ok at low API call rates but completely breaks down when many
-# commands are executed sequentially
+PROCESS: Optional[Thread] = None
+EVENTS = None
 
 
 def start_service():
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    from rpcad import reload_service_modules, fusion
+
+    # make sure any changes are picked up by fusion 360 on restart since the
+    # embedded python interpreter does not unload modules by default
+    reload_service_modules(fusion)
+
     from rpcad.fusion import Fusion360Service
-    from rpyc.utils.server import OneShotServer
+    from rpyc.utils.server import ThreadPoolServer
 
     global SERVER
-    SERVER = Fusion360Service.create_server(server=OneShotServer)
+
+    # use requestBatchSize = 1 since most CAD functions can take a while to
+    # complete, especially when they have to use events to communicate and wait
+    # for the main thread to finish
+    SERVER = Fusion360Service.create_server(server=ThreadPoolServer, requestBatchSize=1)
     SERVER.start()
 
 
 def run(context):
     ui = None
     try:
-        app = adsk.core.Application.get()
+        app: adsk.core.Application = adsk.core.Application.get()  # type: ignore
         ui = app.userInterface
+
+        sys.path.append(os.path.dirname(os.path.abspath(__file__)))
         import rpcad
         import importlib
 
         importlib.reload(rpcad)
-        from rpcad import reload_service_modules, fusion
 
-        # make sure any changes are pickup by fusion 360 on restart
-        reload_service_modules(fusion)
+        from rpcad.fusion import register_events
 
-        # PROCESS = Thread(target=start_service, daemon=True)
-        # PROCESS.start()
-        start_service()
+        global EVENTS
+        EVENTS = register_events()
+
+        PROCESS = Thread(target=start_service, daemon=True)
+        PROCESS.start()
+
+        # need to hold references to events and handlers so that they don't go
+        # out of scope
+        adsk.autoTerminate(False)
 
     except:  # noqa: E722
         if ui:
-            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))  # type: ignore
 
 
-# not used in scripts but left for possible addin functionality
 def stop(context):
     ui = None
     try:
-        app = adsk.core.Application.get()
+        app: adsk.core.Application = adsk.core.Application.get()  # type: ignore
         ui = app.userInterface
 
         if SERVER is not None:
@@ -69,6 +80,11 @@ def stop(context):
         if PROCESS is not None:
             PROCESS.join()
 
+        if EVENTS is not None:
+            from rpcad.fusion import unregister_events
+
+            unregister_events(EVENTS.values())
+
     except:  # noqa: E722
         if ui:
-            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))  # type: ignore
