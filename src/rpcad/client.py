2f315f4aa444fdd21b431c4dfbaf7f5c9961f0d9
dkavolis <12998363+dkavolis@users.noreply.github.com>
2021-07-21 05:39:56 +0100

    add easier way to make commands

--- src/rpcad/client.py
+++ src/rpcad/client.py
@@ -5,11 +5,12 @@
 @Date:                 06-Jun-2020
 @Filename:             client.py
 @Last Modified By:     Daumantas Kavolis
-@Last Modified Time:   14-Jul-2021
+@Last Modified Time:   21-Jul-2021
 """
 
 import os
-from typing import Dict, Union, Any, overload, Iterable, Callable
+from typing import Dict, Union, Any, Iterable, Callable
+from typing import overload as _overload
 import inspect
 
 from rpcad.common import BaseClient
@@ -23,14 +24,33 @@ def remote_call(f: Callable):
     signature = inspect.signature(f)
 
     @wraps(f)
-    def wrapper(self: "BaseClient", *args, **kwargs):
+    def wrapper(self: "BaseClient", *args, as_command: bool = False, **kwargs):
         arguments = signature.bind(self, *args, **kwargs)
         arguments.apply_defaults()
+
+        # special handling for path arguments, should probably add a handler mapping as
+        # decorator argument instead
+        path = arguments.arguments.get("path", None)
+        if path is not None:
+            arguments.arguments["path"] = os.path.abspath(path)
+
+        if as_command:
+            # name may be one of the kwargs so set args and kwargs outside the
+            # constructor
+            command = Command(name=f.__name__)
+            command.args = arguments.args
+            command.kwargs = arguments.kwargs
+            return command
+
         return getattr(self.connection.root, f.__name__)(
             *arguments.args[1:], **arguments.kwargs
         )
 
     return wrapper
+
+
+def overload(f: Callable):
+    return _overload(remote_call(f))
 
 
 class Client(BaseClient):
@@ -42,8 +62,9 @@ class Client(BaseClient):
     def parameters(self) -> Dict[str, Parameter]:  # type: ignore
         pass
 
+    @remote_call
     def open_project(self, path: str) -> None:
-        return self.connection.root.open_project(path=os.path.abspath(path))
+        pass
 
     @remote_call
     def save_project(self) -> None:
@@ -53,10 +74,9 @@ class Client(BaseClient):
     def close_project(self) -> None:
         pass
 
+    @remote_call
     def export_project(self, path: str, *args, **kwargs) -> None:
-        return self.connection.root.export_project(
-            path=os.path.abspath(path), *args, **kwargs
-        )
+        pass
 
     @remote_call
     def set_parameters(self, **kwargs: Union[str, float]) -> None:
@@ -90,11 +110,11 @@ class Client(BaseClient):
     def physical_properties(self, prop, part, accuracy=Accuracy.Medium):  # type: ignore
         pass
 
-    @overload
+    @_overload
     def batch_commands(self, commands: Command) -> Any:
         ...
 
-    @overload
+    @_overload
     def batch_commands(self, commands: Iterable[Command]) -> list:
         ...
 
